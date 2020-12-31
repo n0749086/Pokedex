b55133f1965487ea8a881844c5601bef3c0f3a74
S.Kimura <n0749086@gmail.com>
2020-12-31 22:44:23 +0900

    Update app.py
    modi run argument

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -9,4 +10,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
