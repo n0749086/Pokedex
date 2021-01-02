e0602322c56f039eaa0a2490307e0cc66dbd67be
S.Kimura <n0749086@gmail.com>
2021-01-02 10:39:16 +0900

    add some function
    - add search box(with suggest)
    - load from pickle(modi pickle data)

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 def get_poke_info():
     pokedex = Pokedex()
     poke_name = pokedex.get_poke_name_list()
-    return render_template('poke_info.html', title=poke_name[0] + "のステータス", name=poke_name[0])
+    title = "{}のステータス"
+    return render_template('poke_info.html', title=title, name=poke_name[0], name_list=poke_name)
 
 
 if __name__ == "__main__":
--- pokedex.py
+++ pokedex.py
@@ -1,24 +1,45 @@
-import csv
 import pickle
-
+import csv
 class Pokedex:
-    poke_list = []
+    poke_list = {}
     poke_name_list = []
 
-    def __init__(self):
+    def load_poke_data_pkl(self):
         self.poke_name_list, self.poke_list = pickle.load(open("poke.pkl", "rb"))
-        # with open("pokemon_status.csv") as f:
-        #     reader = csv.reader(f)
-        #     next(reader)
-        #     for row in reader:
-        #         self.poke_list.append(row)
-        #         self.poke_name_list.append(row[1])
-        # with open("poke.pkl", "wb") as f:
-        #     l = [self.poke_name_list, self.poke_list]
-        #     pickle.dump(l, f)
+
+    #図鑑番号,ポケモン名,タイプ１,タイプ２,通常特性１,通常特性２,夢特性,HP,こうげき,ぼうぎょ,とくこう,とくぼう,すばやさ,合計
+    def load_poke_data_csv(self):
+        with open("pokemon_status.csv") as f:
+            reader = csv.reader(f)
+            next(reader)
+            data = {}
+            for row in reader:
+                data["no"] = row[0]
+                name = row[1]
+                data["type1"] = row[2]
+                data["type2"] = row[3]
+                data["abi1"] = row[4]
+                data["abi2"] = row[5]
+                data["abiD"] = row[6]
+                data["HP"] = int(row[7])
+                data["ATK"] = int(row[8])
+                data["DEF"] = int(row[9])
+                data["S_ATK"] = int(row[10])
+                data["S_DEF"] = int(row[11])
+
+                self.poke_list[name] = data
+                self.poke_name_list.append(name)
+        with open("poke.pkl", "wb") as f:
+            l = [self.poke_name_list, self.poke_list]
+            pickle.dump(l, f)
+
+    def __init__(self):
+        self.load_poke_data_pkl()
+        # self.load_poke_data_csv()
 
     def get_poke_name_list(self):
         return self.poke_name_list
+
 
 if __name__ == "__main__":
     poke = Pokedex()
