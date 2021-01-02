785425a5c4e26d2a5783ae0caa400f7191bdbe0a
S.Kimura <n0749086@gmail.com>
2021-01-02 09:58:32 +0900

    rename main function

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from pokedex import Pokedex
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def get_poke_info():
     pokedex = Pokedex()
     poke_name = pokedex.get_poke_name_list()
     return render_template('poke_info.html', title=poke_name[0] + "のステータス", name=poke_name[0])
